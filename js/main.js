e1302f1e48f7f842d671e6298b05e33190e3923b
Alberto Dragoni <albertodragoni@yahoo.it>
2020-02-20 16:06:32 +0100

    funziona!

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+//creazione numeri da 0 a 100 (con ciclo/array)
+//stampare fizz ai multipli di 3
+//stampare buzz ai multipli di 5
+//unire
 
+for (var i = 1; i <= 100; i++) {
+    if (i % 5 ==0 && i % 3 ==0) {
+        console.log('FizzBuzz');
+    } else if (i % 3 ==0) {
+        console.log('Fizz');
+    } else if (i % 5 ==0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
+}
